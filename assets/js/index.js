6313d2ca3fc192a07f7ee339982a3fc6eab2f46f
lakesxo <olaksajanaku@yahoo.com>
2020-10-22 18:00:12 +0100

    updated invoice generator

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+let submit = document.querySelector('input[type="submit"]');
+let displayText = 'Invoice has been saved !'
+
+alat = () => {
+    alert(displayText)
+}
+
+submit.addEventListener('click', alat)
